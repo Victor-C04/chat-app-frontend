47594774de449d060e5ca1f81eda4345b48e9a4a
Victor Chandrasekar <victorroshan4@gmail.com>
2025-04-27 16:39:54 +0530

    api

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development"
-    ? "http://localhost:5001"
-    : "https://chat-app-backend-0663.onrender.com",   // <-- your Render backend URL
+    ? "http://localhost:5001/api"
+    : "https://chat-app-backend-0663.onrender.com/api",   // <-- your Render backend URL
   withCredentials: true, // only if your backend sends cookies (optional for now)
 });
