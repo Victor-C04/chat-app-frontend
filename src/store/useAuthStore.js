93fa9fce20b17340792b6d618738c0896ad66fbe
Victor Chandrasekar <victorroshan4@gmail.com>
2025-04-27 14:35:42 +0530

    cores err

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "https://chat-app-backend-0663.onrender.com";
 
 // Utility functions for token management
 const getAuthToken = () => localStorage.getItem("authToken");
